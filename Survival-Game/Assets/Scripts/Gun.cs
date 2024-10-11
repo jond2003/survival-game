8feb7e65ca80cf3c714786aee290d9bd8d602d09
Jonathan Dennis <144019899+jond2003@users.noreply.github.com>
2024-10-11 03:28:02 +0100

    added gun

---
+++ Survival-Game/Assets/Scripts/Gun.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField] private float damage = 10f;
+    [SerializeField] private float range = 100f;
+
+    [SerializeField] private Camera fpsCam;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
