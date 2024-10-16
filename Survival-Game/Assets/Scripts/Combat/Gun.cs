4cd3231f7f2ab15a8dadc787935e64ebe7442087
Jonathan Dennis <144019899+jond2003@users.noreply.github.com>
2024-10-16 02:07:47 +0100

    final changes

--- Survival-Game/Assets/Scripts/Combat/Gun.cs
+++ Survival-Game/Assets/Scripts/Combat/Gun.cs
@@ -1,27 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Gun : MonoBehaviour
 {
+    [SerializeField] private InputAction inputAxis;
+    [SerializeField] private PlayerInput playerInput;
+
     [SerializeField] private float damage = 10f;
     [SerializeField] private float range = 100f;
-    [SerializeField] private float impactForce = 100f;
+    [SerializeField] private float fireRate = 5f;
+    [SerializeField] private float impactForce = 25f;
 
-    [SerializeField] private Camera fpsCam;
+    private float nextTimeToFire = 0f;
+
+    [SerializeField] private Camera playerCamera;
+
+    private void Awake()
+    {
+        inputAxis = playerInput.actions.FindAction("Fire");
+    }
 
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (inputAxis.IsPressed() && Time.time > nextTimeToFire)
         {
+            nextTimeToFire = Time.time + 1f/fireRate;
             Shoot();
         }
     }
 
-    void Shoot()
+    private void Shoot()
     {
         RaycastHit hit;
-        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name);
 
