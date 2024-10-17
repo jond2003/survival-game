b149d16aaf440b4e7c59ef8b1960833652de4289
Ethan Yeung <ethanyeung513@outlook.com>
2024-10-17 13:06:47 +0100

    Enemy can follow player using navMesh

--- Survival-Game/Assets/Scripts/Enemy/EnemyAI.cs
+++ Survival-Game/Assets/Scripts/Enemy/EnemyAI.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
+
+    [SerializeField] private Transform player;
+    private NavMeshAgent meshAgent;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        meshAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        meshAgent.destination = player.position; 
     }
 }
