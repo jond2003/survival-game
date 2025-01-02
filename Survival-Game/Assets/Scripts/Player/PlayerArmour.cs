792623b8daf03936563bc6f0295b5ad73a48dad7
Ethan Yeung <ethanyeung513@outlook.com>
2025-01-02 16:39:20 +0000

    working armour system

--- Survival-Game/Assets/Scripts/Player/PlayerArmour.cs
+++ Survival-Game/Assets/Scripts/Player/PlayerArmour.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerArmour : MonoBehaviour
@@ -10,29 +11,26 @@ public class PlayerArmour : MonoBehaviour
 
     [SerializeField] private float currentDamageMitigation;
     [SerializeField] private int currentArmourLevel;
-    [SerializeField] private int maxArmourLevel = 5;
+    [SerializeField] private int maxArmourLevel = 3;
+
+    [SerializeField] TMP_Text armourText;
+
+    [SerializeField] CraftingRecipeData armourRecipeData;
 
 
     private List<float> damageMitigationPerLevel = new List<float> //maps damage mitgation to index, e.g. level 1 = 1f
     {
         1.0f, 
         0.90f, 
-        0.85f,  
-        0.75f, 
-        0.65f,   
+        0.85f,
+        0.80f,
     };
 
-    private List<int> steelPerLevel = new List<int> //steel required per level.
-    {
-        3,
-        5,
-        7,
-        9
-    };
 
     private void Start()
     {
         ResetArmour();
+        UpdateArmourText();
     }
     private void Awake()
     {
@@ -52,24 +50,41 @@ public class PlayerArmour : MonoBehaviour
         if (currentArmourLevel < maxArmourLevel)
         {
             currentArmourLevel += 1;
-            currentDamageMitigation = damageMitigationPerLevel[currentArmourLevel - 1]; //-1 because lists start at 0 
+            currentDamageMitigation = damageMitigationPerLevel[currentArmourLevel]; //-1 because lists start at 0 
+            UpdateArmourText();
 
         }
 
     }
+
+
 
    
     public int GetArmourLevel()
     {
         return currentArmourLevel;
     }
+
+    public int GetMaxArmourLevel()
+    {
+        return maxArmourLevel;
+    }
     public float GetDamageMitigation()
     {
         return currentDamageMitigation;
     }
-    public void ResetArmour()
+    private void ResetArmour()
     {
-        currentArmourLevel = 1;
+        currentArmourLevel = 0; //no armour
         currentDamageMitigation = damageMitigationPerLevel[0];
     }
+
+    private void UpdateArmourText()
+    {
+        armourText.text = currentArmourLevel + "/3";
+    }
+
+    
+
+
 }
