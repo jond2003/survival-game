fdef75435b846dfec1a970ed6b24ba28f9b871ff
Jonathan Dennis <144019899+jond2003@users.noreply.github.com>
2025-01-06 01:04:04 +0000

    balancing done

---
+++ Survival-Game/Assets/Asset Data/Spawners/EnemySpawnerData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Enemy Spawner", menuName = "Enemy Spawner")]
+public class EnemySpawnerData : ScriptableObject
+{
+    public int maxEnemies;
+}
